23db2aa35ce93b71127debb86630c46906e4788d
Andrey <ilikeseik@gmail.com>
2022-12-06 12:59:13 +0300

    task 23

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -149,3 +149,14 @@ let c = 3;
 a -= 6;
 b -= 15;
 c -= 1; */
+
+/* 23.
+Convert the assignments for a, b, and c to use the *= operator.
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10; */
