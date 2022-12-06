3522e534f7ddbfa64ce998f2180b488b0231fcbb
Andrey <ilikeseik@gmail.com>
2022-12-06 12:37:43 +0300

    task 10

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -59,3 +59,18 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000; */
+
+/* 9.
+Update the code so it only uses the let keyword.
+
+let catName = "Oliver";
+let catSound = "Meow!"; */
+
+/* 10.
+Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); */
+
