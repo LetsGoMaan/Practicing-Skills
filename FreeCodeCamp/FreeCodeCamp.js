f6e00a9e7074fcc01dd4a6d144bb257ebd2d386d
Andrey <ilikeseik@gmail.com>
2022-12-06 13:25:06 +0300

    task 28

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -192,3 +192,8 @@ FirstLine
 ThirdLine
 
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; */
+
+/* 28.
+Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
+
+const myStr = "This is the start. " + "This is the end."; */
