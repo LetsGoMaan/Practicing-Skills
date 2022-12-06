0758780eb7d821d7174166e0375aeec129d3b59d
Andrey <ilikeseik@gmail.com>
2022-12-06 12:05:47 +0300

    task 2

---
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -0,0 +1,13 @@
+/* 1.
+You should create a in-line style comment that contains at least five letters.
+You should create a multi-line style comment that contains at least five letters.
+*/
+
+// Hello World!
+/* Hello
+    World */
+
+/* 2.
+Use the var keyword to create a variable called myName
+
+var myName; */
