c030f6f2eb9e519910d7a00289e7c4a6efde3a53
Andrey <ilikeseik@gmail.com>
2022-12-06 12:50:18 +0300

    task 19

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -116,3 +116,8 @@ const myDecimal = 99.9; */
 Change the 0.0 so that product will equal 5.0.
 
 const product = 2.0 * 2.5; */
+
+/* 19.
+Change the 0.0 so that quotient will equal to 2.2.
+
+const quotient = 4.4 / 2.0; */
