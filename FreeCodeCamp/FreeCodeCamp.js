bb7ed0ecbc4b20b28cede005f999bd196a4f0eb0
Andrey <ilikeseik@gmail.com>
2022-12-06 12:49:09 +0300

    task 18

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -111,3 +111,8 @@ myVar--; */
 Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
 
 const myDecimal = 99.9; */
+
+/* 18.
+Change the 0.0 so that product will equal 5.0.
+
+const product = 2.0 * 2.5; */
