483b642ce19119b2824ac28911f1cdd3308fb7b5
Andrey <ilikeseik@gmail.com>
2022-12-06 12:19:22 +0300

    task 7

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -36,3 +36,15 @@ Create two new string variables: myFirstName and myLastName and assign them the 
 
 var myFirstName = "Andrey";
 var myLastName = "Prohorov"; */
+
+/* 7.
+Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!"; */
