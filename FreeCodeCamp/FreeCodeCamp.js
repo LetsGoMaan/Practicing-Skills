0dbcedd7597ad31da5f8c1b9904e8da3370d2d05
Andrey <ilikeseik@gmail.com>
2022-12-07 14:50:14 +0300

    task 34

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -234,3 +234,11 @@ let firstLetterOfLastName = "";
 const lastName = "Lovelace";
 
 firstLetterOfLastName = lastName[0]; */
+
+/* 34.
+Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+
+
+let myStr = "Jello World";
+
+myStr = "Hello World";  */
