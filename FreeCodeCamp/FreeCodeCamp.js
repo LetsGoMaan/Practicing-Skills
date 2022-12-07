52886a5a182e443092f297dac4d37c41eaa7cf0a
Andrey <ilikeseik@gmail.com>
2022-12-07 15:32:01 +0300

    task 49

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -353,3 +353,13 @@ const myList = [
     ["Pepper", 3],
     ["Avocado", 2],
   ]; */
+
+/* 49.
+  Create a function called reusableFunction which prints the string Hi World to the dev console.
+Call the function.
+
+function reusableFunction () {
+    console.log("Hi World");
+  }
+  
+  reusableFunction(); */
