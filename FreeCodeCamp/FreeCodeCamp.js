650a7cf8d206eede00e5b68a26ca936227639c3f
Andrey <ilikeseik@gmail.com>
2022-12-07 15:01:39 +0300

    task 38

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -260,3 +260,18 @@ Use bracket notation to find the second-to-last character in the lastName string
 
 const lastName = "Lovelace";
 const secondToLastLetterOfLastName = lastName[lastName.length - 2];  */
+
+/* 38.
+In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+
+You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+
+You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "My " + myNoun + " is really " + myAdjective + " but he is " + myVerb + " so " + myAdverb; 
+ */
