aea3e6c6258ba963525fbe879b9f9c5cdae6cc12
Andrey <ilikeseik@gmail.com>
2022-12-07 15:41:58 +0300

    task 53

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -402,3 +402,17 @@ function fun2() {
   }
   console.log(output);
 } */
+
+/* 53.
+The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. Declare a local variable myVar inside myLocalScope and run the tests.
+
+Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail.
+
+function myLocalScope() {
+ 
+    const myVar = 5;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  console.log('outside myLocalScope', myVar); */
