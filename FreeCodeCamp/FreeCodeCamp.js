55d911ba6ea9232842a26dcbf6e784125fd142c9
Andrey <ilikeseik@gmail.com>
2022-12-07 15:19:05 +0300

    task 44

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -311,3 +311,11 @@ const myArray = [
   ];
   
   const myData = myArray[2][1]; */
+
+/* 44. 
+Push ["dog", 3] onto the end of the myArray variable.
+
+const myArray = [["John", 23], ["cat", 2]];
+
+
+myArray.push(["dog", 3]); */
