583c752e38c4830a1998ff304886dd5cae27a02c
Andrey <ilikeseik@gmail.com>
2022-12-07 14:55:59 +0300

    task 37

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -254,3 +254,9 @@ Use bracket notation to find the last character in the lastName variable.
 
 const lastName = "Lovelace";
 const lastLetterOfLastName = lastName[lastName.length - 1];  */
+
+/* 37.
+Use bracket notation to find the second-to-last character in the lastName string. 
+
+const lastName = "Lovelace";
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];  */
