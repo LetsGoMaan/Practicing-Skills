6d6caa24e9735e6bdbeae4ea189ddfce8cd372b3
Andrey <ilikeseik@gmail.com>
2022-12-07 15:39:23 +0300

    task 52

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -380,3 +380,25 @@ Create a function timesFive that accepts one argument, multiplies it by 5, and r
 function timesFive (a) {
     return a * 5;
   } */
+
+/* 52.
+Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+
+Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
+
+const myGlobal = 10;
+
+function fun1() {
+oopsGlobal = 5;
+}
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+} */
