af81ff1e2d4f903d2ca9536113c4084eaeb7aae1
Andrey <ilikeseik@gmail.com>
2022-12-07 15:22:45 +0300

    task 45

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -319,3 +319,9 @@ const myArray = [["John", 23], ["cat", 2]];
 
 
 myArray.push(["dog", 3]); */
+
+/* 45. 
+Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop(); */
