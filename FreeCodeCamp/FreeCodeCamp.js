e1d0e65e67c84484c306e1b405ca9a06e890e9fb
Andrey <ilikeseik@gmail.com>
2022-12-07 15:43:36 +0300

    task 54

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -416,3 +416,15 @@ function myLocalScope() {
   myLocalScope();
   
   console.log('outside myLocalScope', myVar); */
+
+/* 54.
+Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  const outerWear = "sweater";
+  return outerWear;
+}
+
+myOutfit(); */
