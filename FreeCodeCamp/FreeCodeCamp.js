f1b41ee44dad5a4ffeabe43df7b921c02d15eae3
Andrey <ilieseik@gmail.com>
2022-12-08 20:09:31 +0300

    task 63

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -524,3 +524,15 @@ function compareEquality(a, b) {
 }
 
 compareEquality(10, "10"); */
+
+/* 63.
+Add the inequality operator != in the if statement so that the function will return the string Not Equal when val is not equivalent to 99.
+
+function testNotEqual(val) {
+  if (val != 99) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10); */
