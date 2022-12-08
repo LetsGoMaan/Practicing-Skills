78cf65077ebe82b3fa2f62250fbec084cac87458
Andrey <ilieseik@gmail.com>
2022-12-08 17:26:44 +0300

    task 61

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -500,3 +500,15 @@ function testEqual(val) {
 }
 
 testEqual(10); */
+
+/* 61.
+Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7.
+
+function testStrict(val) {
+  if (val === 7) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10); */
