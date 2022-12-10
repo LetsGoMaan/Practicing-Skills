d6e3333c81861df187c5a9c0ee3ab9e2e2a234a2
Andrey <ilikeseik@gmail.com>
2022-12-10 10:32:34 +0300

    task 72

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -664,3 +664,18 @@ function testElse(val) {
 }
 
 testElse(4); */
+
+/* 72.
+Convert the logic to use else if statements.
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  } 
+}
+
+testElseIf(7); */
