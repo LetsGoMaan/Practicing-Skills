f22dd6108d426455dd1e7f6c43afd2c850d1e892
Andrey <ilikeseik@gmail.com>
2022-12-10 12:28:52 +0300

    task 85

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -964,3 +964,15 @@ const testObj = {
 
 const hatValue = testObj.hat;     
 const shirtValue = testObj.shirt;   */
+
+/* 85.
+Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+const entreeValue = testObj["an entree"];   
+const drinkValue = testObj["the drink"];    */
