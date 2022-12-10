3090b450cf894ec89f06585f5fb3087319489e5e
Andrey <ilikeseik@gmail.com>
2022-12-10 12:26:17 +0300

    task 84

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -952,3 +952,15 @@ const myDog = {
     tails: 1,
     friends: ["human", "wolfs"]
 }; */
+
+/* 84.
+Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+const hatValue = testObj.hat;     
+const shirtValue = testObj.shirt;   */
