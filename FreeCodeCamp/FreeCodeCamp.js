e1fdde2d04662dcc58e99a80b275d11e36339f10
Andrey <ilikeseik@gmail.com>
2022-12-10 11:27:52 +0300

    task 77

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -767,7 +767,6 @@ Write a switch statement which tests val and sets answer for the following condi
 
 function caseInSwitch(val) {
   let answer = "";
-  // Only change code below this line
   switch(val) {
     case 1:
     answer = "alpha";
@@ -782,8 +781,36 @@ function caseInSwitch(val) {
     answer = "delta";
     break;
   }
-  // Only change code above this line
   return answer;
 }
 
 caseInSwitch(1); */
+
+/* 77.
+Write a switch statement to set answer for the following conditions:
+a - apple
+b - bird
+c - cat
+default - stuff
+
+function switchOfStuff(val) {
+  let answer = "";
+  switch(val) {
+    case "a":
+      answer = "apple";
+       break;
+    case "b":
+      answer = "bird";
+       break;
+    case "c":
+      answer = "cat";
+       break;
+    default:
+      answer = "stuff";
+       break;
+  }
+
+  return answer;
+}
+
+switchOfStuff(1); */
