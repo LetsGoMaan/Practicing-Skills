25ae9687a72025847c77dec9acf48c35a6d2bfe6
Andrey <ilikeseik@gmail.com>
2022-12-10 11:15:05 +0300

    task 75

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -721,3 +721,39 @@ if (num < 5) {
 }
 
 testSize(7); */
+
+/* /* 75.
+Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
+
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes >= par + 3) {
+    return "Go Home!";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par && par > 1) {
+    return "Par";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes <= par - 2 && strokes != 1 ) {
+    return "Eagle";
+  } else if (strokes == 1) {
+    return "Hole-in-one!";
+  } else {
+    return "Change Me";
+  }
+
+}
+
+golfScore(5, 4); */
