d4dd673e8eec06d5de97d14455a95e53d07425ef
Andrey <ilikeseik@gmail.com>
2022-12-11 13:53:35 +0300

    exercise 6

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -69,6 +69,40 @@ function greet(name){
   return `Hello, ${name} how are you doing today?`
 } */
 
+/* 6. 
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  let newArr = [];
+  let totalCountPositive = 0;
+  let totalSumNegative = 0;
+    
+      if (input === null || input.length === 0) {
+         return newArr;
+      }
+     
+    input.forEach(item => {
+        if (item > 0) {
+           totalCountPositive++;
+        } else {
+             totalSumNegative+= item;       
+        }
+      });
+      
+      newArr.push(totalCountPositive);
+      newArr.push(totalSumNegative);
+      return newArr;
+    }
+   */
 
 
+
+    
 
