7d8ccfcb82001f12248e7fa3d4476f4c720ea557
Andrey <ilikeseik@gmail.com>
2022-12-11 12:15:18 +0300

    exercise 4

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -45,6 +45,21 @@ let max = function(list){
 console.log(max([-52, 56, 30, 29, -54, 0, -110]));
 console.log(min([-52, 56, 30, 29, -54, 0, -110])); */
 
+/* 4.
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  if(flower1 % 2 == 0 && flower2 % 2 != 0 ) {
+    return true;
+  } else if(flower1 % 2 != 0 && flower2 % 2 == 0 ) {
+    return true;
+  } else {
+    return false;
+  }
+} */
+
 
 
 
