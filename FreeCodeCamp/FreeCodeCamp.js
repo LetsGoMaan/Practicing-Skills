9e60973a9d0c2c4c383fd7038d3d084558b171dc
Andrey <ilieseik@gmail.com>
2022-12-12 19:26:45 +0300

    task 87

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -988,3 +988,15 @@ const testObj = {
 
 const playerNumber = 16; 
 const player = testObj[playerNumber];  */
+
+/* 87.
+Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.
+
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.name = "Happy Coder"; */
