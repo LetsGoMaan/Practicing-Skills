5bb17ec404f81ba5c7ce2e3920780fd8fb5cb432
Andrey <ilieseik@gmail.com>
2022-12-12 19:30:57 +0300

    task 89

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -1012,3 +1012,16 @@ const myDog = {
 };
 
 myDog.bark ="woof"; */
+
+/* 89.
+Delete the tails property from myDog. You may use either dot or bracket notation.
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+delete myDog.tails; */
