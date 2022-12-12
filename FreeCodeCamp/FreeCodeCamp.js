7a970806b88d4282f0aef820b766afff7c573f1b
Andrey <ilieseik@gmail.com>
2022-12-12 19:29:47 +0300

    task 88

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -1000,3 +1000,15 @@ const myDog = {
 };
 
 myDog.name = "Happy Coder"; */
+
+/* 88.
+Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark ="woof"; */
