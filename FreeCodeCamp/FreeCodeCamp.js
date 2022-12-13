6aefd65463139802fb050e7f2d40bcbcf7efbeac
Andrey <ilieseik@gmail.com>
2022-12-13 18:55:00 +0300

    task 101

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -1225,3 +1225,19 @@ let total = 0;
 for( let i = 0; i < myArr.length; i++) {
     total += myArr[i];
 } */
+
+/* 101.
+Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+function multiplyAll(arr) {
+  let product = 1;
+  
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+     product *= arr[i][j];
+  }
+}
+  
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]); */
