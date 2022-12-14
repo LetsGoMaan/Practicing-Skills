c02f5648770b87659229c5d94071c87028d5fb21
Andrey <ilikeseik@gmail.com>
2022-12-14 12:32:43 +0300

    task 109

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -1351,3 +1351,12 @@ function convertToInteger(str) {
  }
  
  convertToInteger("56"); */
+
+/*  109.
+ Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+ function convertToInteger(str) {
+  return parseInt(str, 2);
+  }
+  
+  convertToInteger("10011"); */
