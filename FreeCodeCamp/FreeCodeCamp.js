09b82aefc84bc4a6a37a79bb8d66efe5d7b424c9
Andrey <ilikeseik@gmail.com>
2022-12-14 12:35:01 +0300

    task 110

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -1360,3 +1360,12 @@ function convertToInteger(str) {
   }
   
   convertToInteger("10011"); */
+
+/*   110.
+  Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either the string Equal or the string Not Equal.
+
+  function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+  }
+  
+  checkEqual(1, 2); */
