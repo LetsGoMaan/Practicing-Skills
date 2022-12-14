fc2b66fa2842f9a30c5c0a3d00ad3b720f15cf15
Andrey <ilikeseik@gmail.com>
2022-12-14 12:38:58 +0300

    task 111

--- FreeCodeCamp/FreeCodeCamp.js
+++ FreeCodeCamp/FreeCodeCamp.js
@@ -1369,3 +1369,14 @@ function convertToInteger(str) {
   }
   
   checkEqual(1, 2); */
+
+/*   111.
+  In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.
+
+  function checkSign(num) {
+    return (num > 0) ? "positive"
+    : (num < 0) ? "negative"
+    : "zero";
+  }
+  
+  checkSign(10); */
