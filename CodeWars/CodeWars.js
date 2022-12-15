68ee1eea023666e5f79ca72a0bb5648d6548740d
Andrey <ilikeseik@gmail.com>
2022-12-15 12:31:00 +0300

    exercise 17

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -263,5 +263,10 @@ const reverseSeq = n => {
   return arr;
 }; */
 
+/* 17.
+Make a simple function called greet that returns the most-famous "hello world!".
 
-
+function greet () {
+  return "hello world!";
+}
+ */
