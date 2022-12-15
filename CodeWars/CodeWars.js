70969bc1f5b4efc35e7c26f334e77cc0979f0364
Andrey <ilikeseik@gmail.com>
2022-12-15 12:47:58 +0300

    exercise 20

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -315,3 +315,16 @@ function friend(friends){
   }
   return newArr;
  } */
+
+/*  20.
+ Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+  let multiply = 1;
+  for( let i = 0; i< x.length; i++) {
+    multiply *= x[i];
+  }
+  return multiply;
+} */
