ee50b24b8f6433855c8f5e2ab16897acac96309c
Andrey <ilikeseik@gmail.com>
2022-12-15 11:57:12 +0300

    exercise 13

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -192,3 +192,27 @@ function past(h, m, s){
   return ms;
 } */
 
+/* 13.
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+  let yourBmi = weight / (height * height);
+  if(yourBmi <= 18.5) {
+    return "Underweight";
+  } else if (yourBmi <= 25) {
+    return "Normal";
+  } else if (yourBmi <= 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+} */
+
