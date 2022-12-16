83aaa6820df41f3047a2591e8c5750c5312362e0
LetsGoMaan <ilieseik@gmail.com>
2022-12-16 18:19:49 +0300

    exercise 26

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -401,4 +401,19 @@ function countBy(x, n) {
 }
  */
 
+/* 26.
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+  if ( num === 0) {
+    return 0;
+    } else if (num < 0) {
+      return num;
+    } else {
+      return -num;
+    }
+  } */
+     
+    
+
  
