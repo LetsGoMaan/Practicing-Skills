d919cab937ef8d5e591f8a47f738ba74a17831e2
LetsGoMaan <ilieseik@gmail.com>
2022-12-17 21:04:13 +0300

    exercise 35

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -531,3 +531,15 @@ Very simple, given an integer or a floating-point number, find its opposite.
 function opposite(number) {
   return -number;
 } */
+
+/* 35.
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+const areaOrPerimeter = function(l , w) {
+  if( l == w) {
+    return l * w;
+  } else {
+    return 2 * (l + w);
+  }
+ }; */
