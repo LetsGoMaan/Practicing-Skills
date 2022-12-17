9c3bafd89c66933a105ae7fa2a42730211e147f8
LetsGoMaan <ilieseik@gmail.com>
2022-12-17 21:00:01 +0300

    exercise 34

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -524,3 +524,10 @@ function invert(array) {
  }
 return newArr;
 } */
+
+/* 34.
+Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+  return -number;
+} */
