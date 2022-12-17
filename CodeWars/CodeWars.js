70cf8ad7a0148054ba0186555bd8df3aac9fa1fb
LetsGoMaan <ilieseik@gmail.com>
2022-12-17 12:39:10 +0300

    exercise 29

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -450,3 +450,12 @@ function sumMix(x){
    return sum;
  } */
  
+/* 29.
+Nathan loves cycling.
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+function litres(time) {
+  let totalLitres = Math.floor(time * 0.5);
+  return totalLitres;
+} */
