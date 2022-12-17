f9a53162f84eff2a5eb0e296a1d89b48435efa23
LetsGoMaan <ilieseik@gmail.com>
2022-12-17 09:44:49 +0300

    exercise 27

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -413,7 +413,26 @@ function makeNegative(num) {
       return -num;
     }
   } */
-     
+/* 27.
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+function DNAStrand(dna){
+  let arrDna = dna.split('');
+  let newDna = '';
+  for (let i = 0; i < arrDna.length; i++) {
+    if(dna[i] === "A") {
+     newDna += "T";
+    } else if (dna[i] === "T") {
+       newDna += "A";
+    } else if (dna[i] === "C") {
+       newDna += "G";
+    } else if (dna[i] === "G") {
+       newDna += "C";
+    }
+  }
+   return newDna;
+} */
+
     
 
  
