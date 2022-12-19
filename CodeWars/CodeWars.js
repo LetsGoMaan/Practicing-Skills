c206e115e48b7c91c51be9cbad82f2bb828d108d
LetsGoMaan <ilieseik@gmail.com>
2022-12-19 16:51:11 +0300

    exercise 36

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -543,3 +543,20 @@ const areaOrPerimeter = function(l , w) {
     return 2 * (l + w);
   }
  }; */
+
+/* 36.
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+function getGrade (s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3;
+   if(score < 60) { return  'F'
+   } else if ( score >= 60 && score < 70) {
+     return 'D'
+   }  else if ( score >= 70 && score < 80) {
+     return 'C'
+   }  else if ( score >= 80 && score < 90) {
+     return 'B'
+   }  else if ( score >= 90 && score <= 1000) {
+     return 'A'
+   }
+ } */
