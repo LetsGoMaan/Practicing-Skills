3819e7cf6b465521a7126d408bb316d9755a3804
LetsGoMaan <ilieseik@gmail.com>
2022-12-19 16:56:00 +0300

    exercise 37

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -560,3 +560,20 @@ function getGrade (s1, s2, s3) {
      return 'A'
    }
  } */
+
+/*  37.
+ Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise.
+
+function setAlarm(employed, vacation){
+  if (employed && vacation) {
+    return false;
+  } else if( !employed && vacation ) {
+    return false;
+  } else if(!employed && !vacation) {
+    return false;
+  } else if(employed && !vacation ) {
+   return true;
+   }
+ } */
