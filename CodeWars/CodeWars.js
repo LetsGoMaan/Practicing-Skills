6f3a986421f19e8ca7c49d2c45221b0706fd83c4
LetsGoMan <ilikeseik@gmail.com>
2023-01-13 13:48:53 +0300

    exercise 49

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -751,3 +751,9 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //             break;
 //     }
 // }
+
+// 49. This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+//
+//     function simpleMultiplication(number) {
+//     return number % 2 === 0 ? number * 8 : number * 9
+// }
