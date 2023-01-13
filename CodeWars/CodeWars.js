430e283a84c85b9c22543cc40536e80247022798
LetsGoMan <ilikeseik@gmail.com>
2023-01-13 17:39:43 +0300

    exercise 55

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -838,3 +838,19 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //
 //     return [humanYears,catYears,dogYears];
 // }
+// 55.
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+//
+// const rps = (p1, p2) => {
+//     if(p1 === p2) {
+//         return 'Draw!'
+//     } else if( p1 === 'rock' && p2 === 'scissors') {
+//         return 'Player 1 won!'
+//     }  else if( p1 === 'scissors' && p2 === 'paper') {
+//         return 'Player 1 won!'
+//     } else if(p1 ==='paper' && p2 === 'rock') {
+//         return 'Player 1 won!'
+//     } else {
+//         return 'Player 2 won!'
+//     }
+// }
