0cb07e073b1dec00ee7423db95280e5b060781a0
LetsGoMan <ilikeseik@gmail.com>
2023-01-13 13:10:20 +0300

    exercise 42

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -621,3 +621,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //         return "No"
 //     }
 // }
+
+// 42.
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+//
+//     function cockroachSpeed(s) {
+//     return Math.floor(s * 100000 / 3600);
+// }
