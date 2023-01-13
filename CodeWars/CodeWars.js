b98a1acf73e661a842612680612f02ad6fdb2a63
LetsGoMan <ilikeseik@gmail.com>
2023-01-13 13:27:29 +0300

    exercise 46

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -679,3 +679,18 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     function checkForFactor (base, factor) {
 //     return base % factor === 0 ? true : false;
 // }
+
+// 46.
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+//
+//     LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+//
+//     Note: the input will always be valid.
+//
+//     function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+// }
