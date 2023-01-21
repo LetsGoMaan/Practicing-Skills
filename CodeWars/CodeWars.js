14ba8ef65c1818414c194950581839d07fb789db
LetsGoMan <ilikeseik@gmail.com>
2023-01-21 12:38:20 +0300

    exercise 62

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -959,3 +959,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return Math.abs(volumeA - volumeB);
 // }
+
+// 62.
+// You are given two interior angles (in degrees) of a triangle. Write a function to return the 3rd. Note: only positive integers will be tested.
+//     function otherAngle(a, b) {
+//     return 180 - (a + b);
+// }
+
