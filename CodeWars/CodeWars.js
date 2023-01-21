3d46563f4d15290111fef84e8dc78301b7c8568a
LetsGoMan <ilikeseik@gmail.com>
2023-01-21 12:28:14 +0300

    exercise 60

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -927,3 +927,16 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     function getAge(inputString){
 //     return Number(inputString[0]);
 // }
+
+// 60.
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//
+// function countSheeps(arrayOfSheep) {
+//     let total = 0;
+//     for(let i = 0; i < arrayOfSheep.length; i++) {
+//         if(arrayOfSheep[i] === true) {
+//             total++
+//         }
+//     }
+//     return total;
+// }
