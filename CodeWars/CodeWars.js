b3966f89b727f4c47ab9a95f06ec471d60b83200
Andrey Prokhorov <ilikeseik@gmail.com>
2023-01-25 17:35:52 +0300

    exercise 71

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1084,3 +1084,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // function hoopCount (n) {
 //     return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
 // }
+
+// 71.
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+//
+// function mango(quantity, price){
+//     return quantity * price - Math.floor(quantity / 3) * price;
+// }
