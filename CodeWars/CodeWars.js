3e486fc9f25aa128d64f0d57acab4cb44707f7fd
Andrey Prokhorov <ilikeseik@gmail.com>
2023-01-25 17:40:40 +0300

    exercise 73

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1104,3 +1104,11 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //
 //     return newArray;
 // }
+
+// 73.
+// Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+// When bool is truthy, func1 should be called, otherwise call the func2.
+//
+//     function _if(bool, func1, func2) {
+//     return bool ? func1() : func2()
+// }
