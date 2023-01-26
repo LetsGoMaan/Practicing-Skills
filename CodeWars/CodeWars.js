aa5d125a8000aebdb64603aa35749369b20293a9
LetsGoMan <ilikeseik@gmail.com>
2023-01-26 17:43:33 +0300

    exercise 84

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1214,3 +1214,16 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //         var total = num + 5
 //         return total
 //     }
+
+// 84.
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+//
+//     Return the average of the given array rounded down to its nearest integer.
+//
+//     The array will never be empty.
+//
+//     function getAverage(marks){
+//     const sum = marks.reduce((total, amount) => total + amount)
+//     const average = sum / marks.length
+//     return Math.floor(average);
+// }
