c6c8e6403f80f3fc08396a06349590eeef289d33
LetsGoMan <ilikeseik@gmail.com>
2023-01-26 17:46:53 +0300

    exercise 85

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1227,3 +1227,8 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     const average = sum / marks.length
 //     return Math.floor(average);
 // }
+
+// 85. Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+//     function problem(x){
+//     return typeof x === 'string' ? 'Error' : x * 50 + 6;
+// }
