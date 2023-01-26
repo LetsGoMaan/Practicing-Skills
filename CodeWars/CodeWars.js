38b5033891a9bbe2c7e9975ffb00f3c8e2b0578e
LetsGoMan <ilikeseik@gmail.com>
2023-01-26 17:24:56 +0300

    exercise 79

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1174,3 +1174,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //         {sanity: 'Hello'};
 //     return results
 // }
+
+// 79.
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+//
+//     function sayHello(name) {
+//         return `Hello, ${name}`
+//     }
