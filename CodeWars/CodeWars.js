8c946e32443027a91da19a4ce88cc813363a69c8
LetsGoMan <ilikeseik@gmail.com>
2023-01-26 16:36:26 +0300

    exercise 76

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1140,3 +1140,21 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     let newArr = arr.map(el => `${el} ${el.length}`);
 //     return newArr
 // }
+
+// 76.
+// Inspired by the development team at Vooza, write the function that
+//
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+//     The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+//
+// Note: your function should have a default parameter.
+//
+//     For example(Input --> Output):
+//
+// "anyone else" --> 0
+// "Zach" --> 18
+//
+// function howManyLightsabersDoYouOwn(name) {
+//     return name === 'Zach' ? 18 : 0;
+// }
