2001edbf3c472728217ab871239d44d3186190fb
LetsGoMan <ilikeseik@gmail.com>
2023-02-07 22:38:25 +0300

    exercise 87

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1245,3 +1245,16 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     return Math.sqrt(Math.pow(b.x - a.x, 2) +
 //         Math.pow(b.y - a.y, 2) * 1.0);
 // }
+
+// 87. You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+//
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+//
+//     Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+//
+// Simples!
+//
+//     function fixTheMeerkat(arr) {
+//         return arr.reverse()
+//
+//     }
