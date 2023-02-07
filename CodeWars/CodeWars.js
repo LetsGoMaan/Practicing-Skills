b03f75af9e84c21bbaef777efa4bc4df04f9481b
LetsGoMan <ilikeseik@gmail.com>
2023-02-07 22:06:59 +0300

    exercise 86

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1232,3 +1232,16 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     function problem(x){
 //     return typeof x === 'string' ? 'Error' : x * 50 + 6;
 // }
+
+// 86. This series of katas will introduce you to basics of doing geometry with computers.
+//
+//     Point objects have attributes x and y.
+//
+//     Write a function calculating distance between Point a and Point b.
+//
+//     Tests compare expected result and actual answer with tolerance of 1e-6.
+//
+//     function distanceBetweenPoints(a, b) {
+//     return Math.sqrt(Math.pow(b.x - a.x, 2) +
+//         Math.pow(b.y - a.y, 2) * 1.0);
+// }
