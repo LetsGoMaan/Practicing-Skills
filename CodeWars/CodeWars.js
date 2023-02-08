74e8ffa44e33fa01d8ed158b6bdcdf0209123975
LetsGoMan <ilikeseik@gmail.com>
2023-02-08 13:17:40 +0300

    exercise 100

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1461,3 +1461,15 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     return arr.length
 // }
 
+// 100.
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+//
+//     function multipleOfIndex(array) {
+//     let newArr = []
+//     for( let i = 0; i < array.length; i++) {
+//         if(array[i] % i === 0) {
+//             newArr.push(array[i])
+//         }
+//     }
+//     return newArr;
+// }
