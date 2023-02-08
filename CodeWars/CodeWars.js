d82b61cfe1039a8a57139ced9a9f5166abce569b
LetsGoMan <ilikeseik@gmail.com>
2023-02-08 12:32:58 +0300

    exercise 95

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1387,3 +1387,16 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // function excludingVatPrice(price){
 //     return price === null ? -1:  +(price - (price * 15)/(100 + 15)).toFixed(2)
 // }
+
+// 95.
+// *** No Loops Allowed ***
+//
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+//
+//     Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+//
+// Looking for more, loop-restrained fun? Check out the other kata in the series:
+//
+//     function check(a,x){
+//         return a.includes(x)
+//     };
