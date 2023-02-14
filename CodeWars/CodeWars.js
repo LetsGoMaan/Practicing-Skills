b5571a8221e5e33d3d6e64cb5bdcfc44f433f8f3
Andrey Prokhorov <ilikeseik@gmail.com>
2023-02-14 17:23:34 +0300

    exercise 108

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1565,3 +1565,18 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     function take(arr, n) {
 //     return arr.slice(0 ,n);
 // }
+// 108.
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+//
+// examples:
+//
+//     uniTotal("a") == 97 uniTotal("aaa") == 291
+//
+// function uniTotal (string) {
+//     const arr = string.split('');
+//     let total = 0;
+//     for( let i = 0; i < arr.length; i++) {
+//         total += arr[i].charCodeAt()
+//     }
+//     return total;
+// }
