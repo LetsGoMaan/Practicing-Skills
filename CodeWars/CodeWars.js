d823ba3ef912ac350155a2d79325ed06d27af8f5
Andrey Prokhorov <ilikeseik@gmail.com>
2023-02-17 14:16:08 +0300

    exercise 111

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1617,3 +1617,18 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return count
 // }
+
+// 111.
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+//
+//     Assume that the input n will always be a positive integer.
+//
+//     Examples: (Input --> output)
+//
+// function sumCubes(n){
+//     let total = 0;
+//     for( let i=1; i <= n; i++) {
+//         total += i ** 3
+//     }
+//     return total;
+// }
