aa1e1e9681ec3472713441a1b83edeff907bc637
Andrey Prokhorov <ilikeseik@gmail.com>
2023-02-18 11:15:38 +0300

    exercise 112

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1632,3 +1632,18 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return total;
 // }
+
+// 112.
+//
+// Given an array of integers, your function bubblesortOnce/bubblesort_once/BubblesortOnce (or equivalent, depending on your language's naming conventions) should return a new array equivalent to performing exactly 1 complete pass on the original array. Your function should be pure, i.e. it should not mutate the input array.
+// function bubblesortOnce(a) {
+//     let newArr = [...a]
+//     for(let i =0; i < newArr.length; i++) {
+//         if(newArr[i] > newArr[i + 1]) {
+//             let temp = newArr[i]
+//             newArr[i] = newArr[i + 1]
+//             newArr[i + 1] = temp
+//         }
+//     }
+//     return newArr
+// }
