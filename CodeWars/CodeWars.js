905845c67ff3629c931ea8f940f456726fcf3486
Andrey Prokhorov <ilikeseik@gmail.com>
2023-03-01 14:18:37 +0300

    exercise 119

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1757,3 +1757,28 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // function describeAge(a) {
 //     return a <= 12 ? b + c : a >= 13 && a <= 17 ? b + d : a >= 18 && a <= 64 ? b + e : b + f
 // }
+
+// 119.
+// Task
+// Please refer to two example above and write your first JS function.
+//
+// mission 1:
+//
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+//
+// mission 2:
+//
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+//
+// mission 3:
+//
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+//
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+//
+//     In the end, click "Submit" to submit your code pass this kata.
+//
+//     function helloWorld() {
+//     const str = "Hello World!"
+//     console.log(str)
+// }
