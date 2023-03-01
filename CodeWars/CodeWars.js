a3e5da352949b9451eaa6e4f046f73e2ea377e68
Andrey Prokhorov <ilikeseik@gmail.com>
2023-03-01 14:34:35 +0300

    exercise 120

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1782,3 +1782,8 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     const str = "Hello World!"
 //     console.log(str)
 // }
+
+// 120.
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+//
+// const apple = (x) => x ** 2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
