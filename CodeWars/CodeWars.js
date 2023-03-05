8105b260896da3ec438346658b122bdca9620e70
Andrey Prokhorov <ilikeseik@gmail.com>
2023-03-05 13:36:17 +0300

    exercise 121

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1787,3 +1787,19 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
 //
 // const apple = (x) => x ** 2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+
+// 121.
+// Given a number n, return the number of positive odd numbers below n, EASY!
+//
+//     Examples (Input -> Output)
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+// Expect large Inputs!
+//
+//     function oddCount(n){
+//         if (n % 2 === 0) {
+//             return n / 2
+//         } else {
+//             return Math.floor(n / 2)
+//         }
+//     }
