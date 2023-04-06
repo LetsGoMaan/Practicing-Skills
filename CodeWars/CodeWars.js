b8c266b386c185b82902cd2677a7b174a4948fa7
LetsGoMan <ilikeseik@gmail.com>
2023-04-06 19:30:57 +0300

    exercise 131

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1937,3 +1937,27 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // function move (position, roll) {
 //     return position + roll + roll
 // }
+
+// 131.
+// Your task is to find the first element of an array that is not consecutive.
+//
+//     By not consecutive we mean not exactly 1 larger than the previous element of the array.
+//
+//     E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+//
+// If the whole array is consecutive then return null2.
+//
+//     The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+//
+//     function firstNonConsecutive (arr) {
+//         let result = null
+//         for(let i = 0; i < arr.length-1; i++) {
+//             let firstNumber = arr[i]
+//             let nextNumber = arr[i+1]
+//             if(firstNumber !== nextNumber - 1) {
+//                 result = nextNumber
+//                 return result
+//             }
+//         }
+//         return result
+//     }
