424de0044715a47a82d41d33a7c08f1ffc22bf4d
LetsGoMan <ilikeseik@gmail.com>
2023-04-06 17:50:25 +0300

    exercise 124

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1823,3 +1823,12 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //         }, 0)
 //     }
 // };
+
+// 124.
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+//
+// Can you help her?
+//
+//     Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+//
+// Can you help her?
