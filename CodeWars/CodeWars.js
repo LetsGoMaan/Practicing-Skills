05e4ff3bb910abd2087acefbf1a6bcb8b3610056
LetsGoMan <ilikeseik@gmail.com>
2023-04-06 19:17:18 +0300

    exercise 129

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1892,3 +1892,40 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     function bonusTime(salary, bonus) {
 //     return bonus ? "£" + salary * 10 : "£" + salary
 // }
+
+// 129.
+// The function is not returning the correct values. Can you figure out why?
+//
+//     Example (Input --> Output ):
+//
+//     function getPlanetName(id){
+//         var name;
+//         switch(id){
+//             case 1:
+//                 name = 'Mercury'
+//                 break;
+//             case 2:
+//                 name = 'Venus'
+//                 break;
+//             case 3:
+//                 name = 'Earth'
+//                 break;
+//             case 4:
+//                 name = 'Mars'
+//                 break;
+//             case 5:
+//                 name = 'Jupiter'
+//                 break;
+//             case 6:
+//                 name = 'Saturn'
+//                 break;
+//             case 7:
+//                 name = 'Uranus'
+//                 break;
+//             case 8:
+//                 name = 'Neptune'
+//                 break;
+//         }
+//
+//         return name;
+//     }
