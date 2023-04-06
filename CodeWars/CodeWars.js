582b52818caa4572b74c17b3ce27e084e24c80e3
LetsGoMan <ilikeseik@gmail.com>
2023-04-06 18:12:05 +0300

    exercise 125

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1832,3 +1832,13 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
 //
 // Can you help her?
+
+// 125.
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//     function doubleChar(str) {
+//     let newStr = ''
+//     for(let i = 0; i < str.length; i++) {
+//         newStr+= str[i] + str[i]
+//     }
+//     return newStr
+// }
