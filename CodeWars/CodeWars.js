1856667b7256d819b3c5b5c95e0984e7e1f406a4
LetsGoMan <ilikeseik@gmail.com>
2023-04-28 13:51:34 +0300

    exercise 134

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1979,3 +1979,15 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // function solution(a, b){
 //     return a.length < b.length ? `${a}${b}${a}` :`${b}${a}${b}`
 // }
+
+// 134.
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+//
+//     The returned value must be a string, and have "***" between each of its letters.
+//
+//     You should not remove or add elements from/to the array.
+//
+//     function twoSort(s) {
+//     const newArr = s.sort()
+//     return newArr[0].split('',).join('***')
+// }
