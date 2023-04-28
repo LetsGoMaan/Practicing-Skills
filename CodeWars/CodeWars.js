3ff35681605967aa4255802ece4b031b739127f6
LetsGoMan <ilikeseik@gmail.com>
2023-04-28 13:31:07 +0300

    exercise 133

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1972,3 +1972,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return arr
 // }
+
+// 133.
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+//
+// function solution(a, b){
+//     return a.length < b.length ? `${a}${b}${a}` :`${b}${a}${b}`
+// }
