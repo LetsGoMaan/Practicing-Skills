e23126c6888023ecd182c8b1a1dc89e13e0f1100
LetsGoMan <ilikeseik@gmail.com>
2023-04-28 12:51:18 +0300

    exercise 132

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -1961,3 +1961,14 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //         }
 //         return result
 //     }
+
+// 132.
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+//
+//     function between(a, b) {
+//     let arr = []
+//     for( let i = a; i <= b; i++) {
+//         arr.push(i)
+//     }
+//     return arr
+// }
