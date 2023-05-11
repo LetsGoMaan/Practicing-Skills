ccaa66ab5cd7bb4a37489fe7f5250376d9e6b803
LetsGoMan <ilikeseik@gmail.com>
2023-05-11 11:55:22 +0300

    exercise 138

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2027,3 +2027,17 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     function divisibleBy(numbers, divisor){
 //     return numbers.filter(num => num % divisor === 0 )
 // }
+
+// 138. While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+//
+// checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+//
+// The function receives one parameter health which will always be a whole number between -10 and 10.
+//
+// function checkAlive (health) {
+//     if (health <= 0) {
+//         return false
+//     } else {
+//         return true
+//     }
+// }
