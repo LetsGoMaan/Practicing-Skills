7f2b90c604b26950e2b310a2e358e42f4cf54e2c
LetsGoMan <ilikeseik@gmail.com>
2023-05-11 12:05:30 +0300

    exercise 140

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2065,3 +2065,14 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     else (old >= 30)
 //     return "drink whisky"
 // };
+
+// 140.
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+//
+//     You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+//
+// All values will be positive integers or floats, or zero.
+//
+//     function plural(n) {
+//     return n === 1 ? false : true
+// }
