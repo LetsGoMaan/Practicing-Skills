e33712f7c0576af4cc457e41499e47ccfb4b1f44
LetsGoMan <ilikeseik@gmail.com>
2023-06-10 11:57:19 +0300

    exercise 148

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2212,3 +2212,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return str
 // }
+
+// 148.
+// Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+//
+//     function toBinary(n){
+//     return +n.toString(2);
+// }
