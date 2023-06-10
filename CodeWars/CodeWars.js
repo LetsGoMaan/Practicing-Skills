8aea1e0828bdaa42e683ec71ffcaa55f8b588557
LetsGoMan <ilikeseik@gmail.com>
2023-06-10 12:42:43 +0300

    exercise 151

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2239,3 +2239,20 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     function mouthSize(animal) {
 //     return animal.toLowerCase() === 'alligator' ? "small" : "wide"
 // }
+
+// 151.
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+//
+//     Return a new array with processing every number of the input-array like this:
+//
+// If the number has an integer square root, take this, otherwise square the number.
+//
+//     Example
+//     [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+//
+//     function squareOrSquareRoot(array) {
+//     return newArr = array.map((num) => Number.isInteger(Math.sqrt(num)) ? Math.sqrt(num) : num ** 2 )
+//
+// }
