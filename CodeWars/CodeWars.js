1c16b2bff0d224c7706244f1476a7bb063f6fad5
LetsGoMan <ilikeseik@gmail.com>
2023-07-29 11:43:39 +0300

    exercise 158

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2335,3 +2335,24 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return n
 // }
+
+// 158.
+// Description:
+//     Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+//
+//     Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
+//
+//
+// function remove (string) {
+//     if(string[string.length-1] === '!') {
+//         return string.slice(0,-1)
+//     }
+//
+//     return string
+// }
