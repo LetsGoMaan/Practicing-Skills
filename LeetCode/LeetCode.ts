5d19c341fd34d18e26106b14d6d6953899e6d89c
LetsGoMan <ilikeseik@gmail.com>
2023-08-13 12:08:44 +0300

    problem 2236

--- LeetCode/LeetCode.ts
+++ LeetCode/LeetCode.ts
@@ -2,3 +2,27 @@
 // Given two integers num1 and num2, return the sum of the two integers.
 //
 //     const sum = (num1: number, num2: number): number => num1 + num2
+
+// 2236. Root Equals Sum of Children
+//
+// You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+//
+//     Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+//
+//     /**
+//      * Definition for a binary tree node.
+//      * class TreeNode {
+//      *     val: number
+//      *     left: TreeNode | null
+//      *     right: TreeNode | null
+//      *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//      *         this.val = (val===undefined ? 0 : val)
+//      *         this.left = (left===undefined ? null : left)
+//      *         this.right = (right===undefined ? null : right)
+//      *     }
+//      * }
+//      */
+//
+//     function checkTree(root: TreeNode | null): boolean {
+//     return root.val === root.left.val + root.right.val
+// };
