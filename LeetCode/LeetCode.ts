ba4262ef21a729337809502030cbdcfa8993c564
LetsGoMan <ilikeseik@gmail.com>
2023-08-13 11:42:12 +0300

    problem 2235/ts

--- LeetCode/LeetCode.ts
+++ LeetCode/LeetCode.ts
@@ -1,4 +1,4 @@
 // 2235. Add Two Integers
 // Given two integers num1 and num2, return the sum of the two integers.
 //
-//     const sum =(num1,num2) => num1 + num2
+//     const sum = (num1: number, num2: number): number => num1 + num2
