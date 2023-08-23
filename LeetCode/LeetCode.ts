3245c9aea4dd9b560eda58e7f06abda84a425ff7
LetsGoMan <ilikeseik@gmail.com>
2023-08-23 13:03:53 +0300

    problem 1342

--- LeetCode/LeetCode.ts
+++ LeetCode/LeetCode.ts
@@ -74,3 +74,17 @@
 //     }
 //     return arr
 // };
+
+// 1342. Number of Steps to Reduce a Number to Zero
+// Given an integer num, return the number of steps to reduce it to zero.
+//
+//     In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+//
+//     function numberOfSteps(num: number): number {
+//     let steps = 0
+//     while(num > 0) {
+//         num = num % 2 === 0 ? num / 2 : num - 1
+//         steps++
+//     }
+//     return steps
+// };
