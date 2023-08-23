d7fce0530d14f2edab275a4be0cc925ae2bab4ce
LetsGoMan <ilikeseik@gmail.com>
2023-08-23 13:03:29 +0300

    problem 412

--- LeetCode/LeetCode.ts
+++ LeetCode/LeetCode.ts
@@ -47,3 +47,30 @@
 //     }
 //     return richestCustomer
 // };
+
+
+// 412. Fizz Buzz
+
+
+// Given an integer n, return a string array answer (1-indexed) where:
+//
+//     answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+//
+//     function fizzBuzz(n: number): string[] {
+//     let arr = []
+//     for(let i = 1; i <= n; i++ ) {
+//         if(i % 3 === 0 && i % 5 === 0) {
+//             arr.push('FizzBuzz')
+//         } else if (i % 3 === 0) {
+//             arr.push('Fizz')
+//         } else if (i % 5 === 0) {
+//             arr.push('Buzz')
+//         } else {
+//             arr.push(i.toString())
+//         }
+//     }
+//     return arr
+// };
