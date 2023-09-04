29748111d8a3de095aab76e2a4dc591f69efa95c
LetsGoMan <ilikeseik@gmail.com>
2023-09-04 17:56:36 +0300

    problem 1

--- LeetCode/LeetCode.ts
+++ LeetCode/LeetCode.ts
@@ -117,3 +117,21 @@
 //
 //     return slow;
 // };
+
+// 1. Two Sum
+//
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+//     You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//     You can return the answer in any order.
+//
+//     function twoSum(nums: number[], target: number): number[] {
+//     let indices = []
+//     for(let i = 0; i<nums.length; i++) {
+//         if(nums[i] + nums[i + 1] === target) {
+//             indices.push(i, i+1)
+//         }
+//     }
+//     return indices
+// };
