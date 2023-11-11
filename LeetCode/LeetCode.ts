e545399b6a4f069e62af842e2f05dff9df9cdb02
LetsGoMan <ilikeseik@gmail.com>
2023-11-11 13:12:57 +0300

    problem 2723

--- LeetCode/LeetCode.ts
+++ LeetCode/LeetCode.ts
@@ -200,3 +200,15 @@
 // function argumentsLength(...args: JSONValue[]): number {
 //     return args.length
 // };
+
+// 2723. Add Two Promises
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+//
+//     type P = Promise<number>
+//
+// async function addTwoPromises(promise1: P, promise2: P): P {
+//     return Promise.all([promise1, promise2]).then(([result1,result2]) => {
+//         const sum = result1 + result2
+//         return sum
+//     })
+// };
