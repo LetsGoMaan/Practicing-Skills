80861354df8a4e3c53859f9f90800488723e9010
LetsGoMan <ilikeseik@gmail.com>
2023-11-12 19:07:59 +0300

    exercise 182

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2713,3 +2713,15 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // function usdcny(usd) {
 //     return `${(usd * 6.75).toFixed(2)} Chinese Yuan`
 // }
+
+// 182. You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
+//
+// You can not use multiplier "*" operator.
+//
+//     function billboard(name, price = 30){
+//     let cost = 0
+//     for(let i =0; i<name.length; i++) {
+//         cost+=price
+//     }
+//     return cost
+// }
