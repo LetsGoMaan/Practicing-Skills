fa79043b81b04953c3b9bc35fc0d3f3418d780cc
LetsGoMan <ilikeseik@gmail.com>
2023-11-12 18:34:12 +0300

    exercise 181

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2708,3 +2708,8 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     let r = 2 * A / Math.PI
 //     return r * r
 // }
+// 181. Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+//
+// function usdcny(usd) {
+//     return `${(usd * 6.75).toFixed(2)} Chinese Yuan`
+// }
