d04832081086b97ae2921888deeec927cfc23e7d
LetsGoMan <ilikeseik@gmail.com>
2023-11-12 12:09:17 +0300

    exercise 171

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2518,3 +2518,32 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     var websites = []
 // websites.length = 1000
 // websites.fill('codewars', 0, 1000)
+
+// 171. Write a function that will check if two given characters are the same case.
+//
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+// Examples
+// 'a' and 'g' returns 1
+//
+// 'A' and 'C' returns 1
+//
+// 'b' and 'G' returns 0
+//
+// 'B' and 'g' returns 0
+//
+// '0' and '?' returns -1
+//
+// function sameCase(a, b){
+//     const lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//     const uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//     const allLetters = lowercaseLetters.concat(uppercaseLetters)
+//     if(!allLetters.includes(a) || !allLetters.includes(b)) {
+//         return -1
+//     } else if ((lowercaseLetters.includes(a) && lowercaseLetters.includes(b)) || (uppercaseLetters.includes(a) && uppercaseLetters.includes(b))) {
+//         return 1
+//     } else {
+//         return 0
+//     }
+// }
