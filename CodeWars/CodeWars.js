5a0f540a342411872a3ec9d1ec54050d3eafb198
LetsGoMan <ilikeseik@gmail.com>
2023-11-12 10:14:35 +0300

    exercise 168

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2497,3 +2497,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // function printArray(array){
 //     return array.join(',')
 // }
+// 168.
+// Given the total number of rows and columns in the theater (nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.
+// // function seatsInTheater(nCols, nRows, col, row) {
+// //     const newCols = nCols - col
+// //     const newRows = nRows - row
+// //     return newCols * newRows + newRows
+// // }
