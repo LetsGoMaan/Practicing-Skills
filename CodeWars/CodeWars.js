93bfc97cc2886522d84656cb46b61552f6d459f4
LetsGoMan <ilikeseik@gmail.com>
2023-11-12 18:26:48 +0300

    exercise 180

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2700,3 +2700,11 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return str
 // }
+
+// 180.
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals
+//
+// function squareArea(A){
+//     let r = 2 * A / Math.PI
+//     return r * r
+// }
