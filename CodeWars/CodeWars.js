2b174606dcfeb0cd553e8b8a6d9447f72cf9e514
LetsGoMan <ilikeseik@gmail.com>
2023-11-12 18:09:59 +0300

    exercise 179

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2685,3 +2685,18 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     //the answer should be "yes" or "no"
 //     return "yes";
 // }
+
+// 179.
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+//
+//     E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+//
+// function tripleTrouble(one, two, three){
+//     let str = ''
+//     for (let i = 0; i<one.length;i++) {
+//         str+=one[i]
+//         str+=two[i]
+//         str+=three[i]
+//     }
+//     return str
+// }
