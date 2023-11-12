6150892b676f65c5534293db3f43c69f496215bf
LetsGoMan <ilikeseik@gmail.com>
2023-11-12 17:56:37 +0300

    exercise 177

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2628,3 +2628,30 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     }
 //     return obj[newStr] ? obj[newStr] : 'Beer'
 // }
+
+// 177. I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+//
+// function getLength(arr){
+//     //return length of arr
+//     return arr.length
+// }
+// function getFirst(arr){
+//     //return the first element of arr
+//     return arr[0]
+// }
+// function getLast(arr){
+//     //return the last element of arr
+//     return arr[arr.length - 1 ]
+// }
+// function pushElement(arr){
+//     var el=1;
+//
+//     arr.push(el)
+//     return arr
+// }
+// function popElement(arr){
+//     //pop an element from arr
+//
+//     arr.pop()
+//     return arr
+// }
