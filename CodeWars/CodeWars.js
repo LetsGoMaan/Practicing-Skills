9290145fbdacc2c53d130490647476324df6088b
LetsGoMan <ilikeseik@gmail.com>
2023-11-13 14:02:42 +0300

    exercise 189

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2832,3 +2832,15 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     },0)
 //     return firstArrSquarSum > secondArrCubeSum
 // }
+
+// 189. Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+//
+//     The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+//
+//     Upper or lower case letter does not matter -- "eNglisH" is also correct.
+//
+//     Return value as boolean values, true for the string to contains "English", false for it does not.
+//
+//     function spEng(sentence){
+//     return sentence.toLowerCase().includes('english')
+// }
