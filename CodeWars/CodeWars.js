65a5ab5faf91c06f76d6c420d997b34127a0ea6a
LetsGoMan <ilikeseik@gmail.com>
2023-11-13 13:48:28 +0300

    exercise 187

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2815,3 +2815,7 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // //   }
 // //     }
 // }
+
+// 187. This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+//
+// const solve = (x, y) => x / y
