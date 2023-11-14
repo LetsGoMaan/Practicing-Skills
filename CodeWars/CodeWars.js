caf5852d02b7a9b8aaabe8e1fb949788449d7abe
LetsGoMan <ilikeseik@gmail.com>
2023-11-14 14:48:08 +0300

    exercise 199

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2992,4 +2992,24 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     return c.charCodeAt(0)
 // }
 
+// 199. An AI has infected a text with a character!!
+//
+//     This text is now fully mutated to this character.
+//
+//     If the text or the character are empty, return an empty string.
+//     There will never be a case when both are empty as nothing is going on!!
+//
+//     Note: The character is a string of length 1 or an empty string.
+//
+//     function contamination(text, char){
+//     let newText = ''
+//     if( text.length === 0 || char.length === 0) {
+//         return ''
+//     } else {
+//         for(let i =0; i<text.length;i++) {
+//             newText+=char
+//         }
+//     }
+//     return newText
+// }
 
