055ec72beb09853338033463d10bb426a17b573d
LetsGoMan <ilikeseik@gmail.com>
2023-11-14 13:33:01 +0300

    exercise 193

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -2898,3 +2898,18 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 // Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
 //
 //     const combineNames = (firstName,lastName) =>`${firstName} ${lastName}`
+// 193.
+// When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. The petals start to fall in late April.
+//
+// Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it takes 80 seconds for the petal to reach the ground from a certain branch.
+//
+//     Write a function that receives the speed (in cm/s) of a petal as input, and returns the time it takes for that petal to reach the ground from the same branch.
+//
+//     Notes:
+//
+// The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
+//     Pay attention to the data types.
+//     If the initial velocity is non-positive, the return value should be 0
+//
+// const sakuraFall = (v) => v <= 0 ? 0 : 80 * 5 / v
+
