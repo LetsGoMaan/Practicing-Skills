aa1f909f23b7c39c87bbdc37b1c1178be9de08f5
LetsGoMan <ilikeseik@gmail.com>
2023-11-17 14:24:26 +0300

    exercise 207

--- CodeWars/CodeWars.js
+++ CodeWars/CodeWars.js
@@ -3148,3 +3148,11 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     return values.filter( val => val%2 );
 // }
 
+// 207.
+//
+// ou will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+//
+//     const ArrowFunc = function(arr) {
+//     return arr.map(val => String.fromCharCode(val) ).join(''); //Complete this function
+// }
+
